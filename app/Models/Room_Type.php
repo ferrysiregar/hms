d60e76b7223bc32bb513d6e3e33d93f3b5ddb4cd
Ferryanto Siregar <ferrysiregar.com@gmail.com>
2022-10-17 20:56:26 +0700

    Revert "dev"

--- app/Models/Room_Type.php
+++ app/Models/Room_Type.php
@@ -1,160 +1,137 @@
-<?php
-
+<?php 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-
-class Room_Type extends Model
+class Room_Type extends Model 
 {
-
+	
 
 	/**
-	 * The table associated with the model.
-	 *
-	 * @var string
-	 */
+     * The table associated with the model.
+     *
+     * @var string
+     */
 	protected $table = 'room_type';
-
+	
 
 	/**
-	 * The table primary key field
-	 *
-	 * @var string
-	 */
+     * The table primary key field
+     *
+     * @var string
+     */
 	protected $primaryKey = 'id';
-
+	
 
 	/**
-	 * Table fillable fields
-	 *
-	 * @var array
-	 */
+     * Table fillable fields
+     *
+     * @var array
+     */
 	protected $fillable = [
-		'room_type', 'floor_id', 'desc_type'
+		'room_type','floor_id','desc_type'
 	];
-
-	protected $with = ['room', 'floor'];
-
+	
 
 	/**
-	 * Indicates if the model should be timestamped.
-	 *
-	 * @var bool
-	 */
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
 	public $timestamps = false;
-
-
-
-	public function room()
-	{
-		return $this->hasMany(Room::class, 'room_type_id');
-	}
-
-	public function floor()
-	{
-		return $this->belongsTo(Room_Floor::class, 'floor_id');
-	}
-
+	
 
 	/**
-	 * Set search query for the model
+     * Set search query for the model
 	 * @param \Illuminate\Database\Eloquent\Builder $query
 	 * @param string $text
-	 */
-	public static function search($query, $text)
-	{
+     */
+	public static function search($query, $text){
 		//search table record 
 		$search_condition = '(
 				room_type.room_type LIKE ?  OR 
 				room_type.desc_type LIKE ? 
 		)';
 		$search_params = [
-			"%$text%", "%$text%"
+			"%$text%","%$text%"
 		];
 		//setting search conditions
 		$query->whereRaw($search_condition, $search_params);
 	}
-
+	
 
 	/**
-	 * return list page fields of the model.
-	 * 
-	 * @return array
-	 */
-	public static function listFields()
-	{
-		return [
+     * return list page fields of the model.
+     * 
+     * @return array
+     */
+	public static function listFields(){
+		return [ 
 			"room_type.id AS id",
 			"room_type.room_type AS room_type",
 			"room_type.floor_id AS floor_id",
 			"room_floor.floor AS room_floor_floor",
-			"room_type.desc_type AS desc_type"
+			"room_type.desc_type AS desc_type" 
 		];
 	}
-
+	
 
 	/**
-	 * return exportList page fields of the model.
-	 * 
-	 * @return array
-	 */
-	public static function exportListFields()
-	{
-		return [
+     * return exportList page fields of the model.
+     * 
+     * @return array
+     */
+	public static function exportListFields(){
+		return [ 
 			"room_type.id AS id",
 			"room_type.room_type AS room_type",
 			"room_type.floor_id AS floor_id",
 			"room_floor.floor AS room_floor_floor",
-			"room_type.desc_type AS desc_type"
+			"room_type.desc_type AS desc_type" 
 		];
 	}
-
+	
 
 	/**
-	 * return view page fields of the model.
-	 * 
-	 * @return array
-	 */
-	public static function viewFields()
-	{
-		return [
+     * return view page fields of the model.
+     * 
+     * @return array
+     */
+	public static function viewFields(){
+		return [ 
 			"id",
 			"room_type",
 			"floor_id",
-			"desc_type"
+			"desc_type" 
 		];
 	}
-
+	
 
 	/**
-	 * return exportView page fields of the model.
-	 * 
-	 * @return array
-	 */
-	public static function exportViewFields()
-	{
-		return [
+     * return exportView page fields of the model.
+     * 
+     * @return array
+     */
+	public static function exportViewFields(){
+		return [ 
 			"id",
 			"room_type",
 			"floor_id",
-			"desc_type"
+			"desc_type" 
 		];
 	}
-
+	
 
 	/**
-	 * return edit page fields of the model.
-	 * 
-	 * @return array
-	 */
-	public static function editFields()
-	{
-		return [
+     * return edit page fields of the model.
+     * 
+     * @return array
+     */
+	public static function editFields(){
+		return [ 
 			"id",
 			"room_type",
 			"floor_id",
-			"desc_type"
+			"desc_type" 
 		];
 	}
 }
